8294ccfa64fab5103f61250995ae1176e7362c55
Andreas Jung <info@zopyx.com>
2026-05-07 19:08:51 +0200

    updated

--- block-and-list-rs/BlueskyModeration/Features/Accounts/Models/AccountSession.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct AccountSession: Sendable, Equatable {
-    let accountId: UUID
-    let accessJwt: String
-    let did: String
-    let handle: String
-    let pdsEndpoint: String
-}
